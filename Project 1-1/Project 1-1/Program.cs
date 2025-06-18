e6bd5ad52caf2699fa8f74414d6d730aa40ba493
PL.D De Silva <169179183+DilshanDe@users.noreply.github.com>
2025-06-18 19:27:47 +0530

    Initial commit

---
+++ Project 1-1/Project 1-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your FristName: ");
+            String fristName = Console.ReadLine();
+            Console.WriteLine("Enter Your LastName : ");
+            String LastName = Console.ReadLine();
+            Console.WriteLine("Enter your Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Weight: ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Your FristName is: " + fristName);
+            Console.WriteLine("Your lastName is:" + LastName);
+            Console.WriteLine("Your Age is :" + age);
+            Console.WriteLine("Your Weight  is : " + weight);
+
+            Console.ReadLine();
+        }
+    }
+}
